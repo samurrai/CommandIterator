0b2ebf58b68b8b1175b0bc9ff32b6937dd440314
samurrai <samurrrai.r@gmail.com>
2019-07-11 00:02:01 +0600

    intial

---
+++ CommandPattern/CommandPattern/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandPattern
+{
+    // Интерфейс Команды объявляет метод для выполнения команд.
+    public interface ICommand
+    {
+        void Execute();
+    }
+
+    class SendData : ICommand
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Sended");
+        }
+    }
+
+    class Invoker
+    {
+        private ICommand _onStart;
+
+        private ICommand _onFinish;
+
+        // Инициализация команд
+        public void SetOnStart(ICommand command)
+        {
+            this._onStart = command;
+        }
+
+        public void SetOnFinish(ICommand command)
+        {
+            this._onFinish = command;
+        }
+
+        // Отправитель не зависит от классов конкретных команд и получателей.
+        // Отправитель передаёт запрос получателю косвенно, выполняя команду.
+        public void DoSomethingImportant()
+        {
+            _onStart.Execute();
+            _onFinish.Execute();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Клиентский код может параметризовать отправителя любыми
+            // командами.
+            Invoker invoker = new Invoker();
+
+            invoker.SetOnStart(new SendData());
+            invoker.SetOnFinish(new SendData());
+
+            invoker.DoSomethingImportant();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ IteratorPattern/IteratorPattern/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("IteratorPattern")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("IteratorPattern")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("a7ef2fca-701d-44d0-b632-3cc91bb45125")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
